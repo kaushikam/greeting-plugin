ce32cdaf7e434137f22fe05f8325f49938a3a753
kaushik <kaushikam@gmail.com>
2020-01-22 10:46:04 +0530

    Added java gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,26 @@
 plugins {
 	id("org.jetbrains.kotlin.jvm").version("1.3.61")
+	id("java-gradle-plugin")
 }
 
 repositories {
 	jcenter()
 }
 
+gradlePlugin {
+	plugins {
+		register("greetingPlugin") {
+			id = "org.example.greeting"
+			implementationClass = "org.example.greeting.GreetingPlugin"
+		}
+
+		removeIf {
+			name == "java"
+		}
+	}
+}
+
+
 dependencies {
-	implementation(gradleApi())
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
